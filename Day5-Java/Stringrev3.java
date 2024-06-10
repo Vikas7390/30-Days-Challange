4c8211df9cc0a099eee92639c4c84cfda86eef28
Vikas Kamsani <112812406+Vikas7390@users.noreply.github.com>
2024-06-10 22:55:28 +0530

    Stringrev3.java

---
+++ Day5-Java/Stringrev3.java
@@ -0,0 +1,8 @@
+public class Stringrev3 {
+    public static void main(String[] args) {
+        // using string buffer class
+        String str = "ABCD";
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+    }
+}
