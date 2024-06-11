fd4a9b0aeb2c5e888c9932dbe4a0848307bc2209
Vikas Kamsani <112812406+Vikas7390@users.noreply.github.com>
2024-06-11 22:07:33 +0530

    Pattern1.java

---
+++ Day6-Java/Pattern1.java
@@ -0,0 +1,9 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        //PATTERNS
+        int n = 5;
+        for (int i=1; i<= n; i++){
+            System.out.println("*");
+        }
+    }
+}
