2291d636131fb10b0b9f32d3f2466c9c43f405b1
Vikas Kamsani <112812406+Vikas7390@users.noreply.github.com>
2024-06-11 22:07:58 +0530

    Pattern2.java

---
+++ Day6-Java/Pattern2.java
@@ -0,0 +1,11 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        int N= 5;
+        for (int row=1; row <=N; row = row+1){
+            for(int col=1; col<=N; col=col+1){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
